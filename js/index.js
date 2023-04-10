c7e9a2e1aa524180f31a79f6b1919b7fa1042fee
YunPrincipal <xownsdmgk@gmail.com>
2023-04-10 13:20:19 +0900

    * 230410 - 기원 정보 > 원신v3.6 전반기 픽업 내용 업데이트 * 230411 - 홈 > 원신v3.6 전반기 픽업 배너 이미지 업데이트

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var today = new Date();
-var end_day = new Date ("2023-03-21 19:00:00"); //현재 픽업 끝나는 시간
-var next_end_day = new Date("2023-04-11 16:00:00"); //다음 픽업 끝나는 시간
+var end_day = new Date ("2023-04-11 16:00:00"); //현재 픽업 끝나는 시간
+var next_end_day = new Date("2023-05-02 19:00:00"); //다음 픽업 끝나는 시간
 
 var day_HTML = document.getElementById('day_interval');
 var day_HTML2 = document.getElementById('day_interval1');
@@ -10,8 +10,8 @@ if (end_day >= today){
   var wish_img1 = document.getElementById('wish_img_1');
   var wish_img2 = document.getElementById('wish_img_2');
 
-  wish_img1.src = "img/index/v3.50_char.jpg"; //현재 픽업 배너
-  wish_img2.src = "img/index/v3.50_weapon.jpg";
+  wish_img1.src = "img/index/v3.51_char.jpg"; //현재 픽업 배너
+  wish_img2.src = "img/index/v3.51_weapon.jpg";
 
 }
   else{
@@ -20,8 +20,8 @@ if (end_day >= today){
 
   end_day = next_end_day;
 
-  wish_img1.src = "img/index/v3.51_char.jpg"; //다음 픽업 배너
-  wish_img2.src = "img/index/v3.51_weapon.jpg";
+  wish_img1.src = "img/index/v3.60_char.jpg"; //다음 픽업 배너
+  wish_img2.src = "img/index/v3.60_weapon.jpg";
 
 }
 console.log('today :'+today);
